be1e9d9a4abce52a7d20fb0ec21de6b375e67d53
fongzhizhi <1837486996@qq.com>
2024-05-20 18:16:40 +0800

    sample: drawTwoRects

--- src/app.ts
+++ src/app.ts
@@ -1,39 +1,12 @@
+import { drawTwoRects } from "./samples/cavas-tutotail-mdn/01.drawTwoRects";
 import "./styles/main.less";
-import { printStyleLog } from "./utils/util";
-import axios from "axios";
-import { marked } from "marked";
 
 window.onload = () => {
-  loadReadme();
-  doSomething();
+	main();
 };
 
-function loadReadme() {
-  axios
-    .get("http://localhost:3030/readme")
-    .then((res) => {
-      if (res && res.data) {
-        const readMeHtml = marked(res.data);
-        document.getElementById("readme").innerHTML = readMeHtml;
-      }
-    })
-    .catch((err) => {
-      printStyleLog("Server Error", err);
-    });
-}
-
-/**
- * print something
- */
-function doSomething() {
-  printStyleLog(
-    "Jinx",
-    {
-      name: "Jinx",
-      age: 21,
-    },
-    {
-      color: "#41b883",
-    }
-  );
+/**入口函数 */
+function main() {
+	const canvasSelector = '#my-canvas';
+	drawTwoRects(canvasSelector);	
 }
